0d41e1430e81ce605f64f4d5e3216ce8c0156991
DianaTarnu <diana.tarnu@gmail.com>
2020-10-30 18:00:18 +0200

    Add Data Binding: Data

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -11,6 +11,8 @@ import com.example.aboutme.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private var myName: MyName = MyName("Diana Tarnu")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -22,6 +24,8 @@ class MainActivity : AppCompatActivity() {
 
         //with Data Binding
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
         binding.doneButton.setOnClickListener{
             addNickname(it)
         }
@@ -39,6 +43,7 @@ class MainActivity : AppCompatActivity() {
 
         //with data binding
         binding.apply {     //make it easier to read
+            myName?.nickname = nicknameEdit.text.toString()
             invalidateAll()     //refresh the UI with the new data to recreate the correct data
             binding.nicknameText.text = binding.nicknameEdit.text
             binding.nicknameEdit.visibility = View.GONE
